ed09192d677c94e33bac97d6d9456f2dff24d88b
Sean <e0406739@u.nus.edu>
2020-10-16 16:32:05 +0800

    test case for title description test

--- src/test/java/seedu/address/ui/TitleDescriptionTest.java
+++ src/test/java/seedu/address/ui/TitleDescriptionTest.java
@@ -5,6 +5,10 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import seedu.address.ui.textstyle.TitleDescription;
 
 public class TitleDescriptionTest {
@@ -33,5 +37,29 @@ public class TitleDescriptionTest {
 
         // different title and description -> returns false
         assertFalse(titleDescription.equals(TitleDescription.createTitleDescription("HELLO", "WORLD")));
+    }
+    
+    @Test
+    public void getTitle_true_success() {
+        assertTrue(TitleDescription.createTitleDescription("Hello", "World").getTitle().getText().equals("Hello"));
+        assertTrue(TitleDescription.createTitleDescription("Title", "World").getTitle().getText().equals("Title"));
+    }
+
+    @Test
+    public void getTitle_false_success() {
+        assertFalse(TitleDescription.createTitleDescription("Hello", "World").getTitle().getText().equals("Title"));
+        assertFalse(TitleDescription.createTitleDescription("Title", "World").getTitle().getText().equals("Hello"));
+    }
+
+    @Test
+    public void getDescription_true_success() {
+        assertTrue(TitleDescription.createTitleDescription("Hello", "World").getDescription().getText().equals("World"));
+        assertTrue(TitleDescription.createTitleDescription("Hello", "Description").getDescription().getText().equals("Description"));
+    }
+
+    @Test
+    public void getDescription_false_success() {
+        assertFalse(TitleDescription.createTitleDescription("Hello", "Description").getDescription().getText().equals("World"));
+        assertFalse(TitleDescription.createTitleDescription("Hello", "World").getDescription().getText().equals("Description"));
     }
 }
