91bb61210727ef7993d1b6401999c1f5befa46d0
Sean <e0406739@u.nus.edu>
2020-10-18 01:41:24 +0800

    fix checkstyle

--- src/main/java/seedu/address/logic/parser/view/ViewCommandParser.java
+++ src/main/java/seedu/address/logic/parser/view/ViewCommandParser.java
@@ -1,6 +1,5 @@
 package seedu.address.logic.parser.view;
 
-import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_ITEM_TYPE;
 import static seedu.address.logic.parser.util.GeneralParserUtil.checkCommandDetailsIsNotBlank;
 import static seedu.address.logic.parser.util.GeneralParserUtil.getCommandDetails;
