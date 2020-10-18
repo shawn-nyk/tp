33a5f0be6bd13c9b42b17915a3687cb63480b7e3
Sean <e0406739@u.nus.edu>
2020-10-18 17:40:05 +0800

    Reset ApplicationViewIndex

--- src/main/java/seedu/address/logic/commands/find/FindApplicationCommand.java
+++ src/main/java/seedu/address/logic/commands/find/FindApplicationCommand.java
@@ -4,6 +4,7 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.model.util.ItemUtil.APPLICATION_ALIAS;
 
 import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
@@ -29,7 +30,7 @@ public class FindApplicationCommand extends FindCommand {
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
         model.getApplicationList().updateFilteredItemList(predicate);
-
+        model.setApplicationViewIndex(Index.fromZeroBased(1));
         return new CommandResult(
             String.format(Messages.MESSAGE_FIND_APPLICATION_SUCCESS,
                 model.getApplicationList().getFilteredItemList().size()));
