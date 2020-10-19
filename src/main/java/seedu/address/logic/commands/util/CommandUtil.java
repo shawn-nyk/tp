a6a9779ee7b68794ac327d709c2ebade290e2a78
shawn-nyk <shawnngyongkwang@gmail.com>
2020-10-19 23:08:56 +0800

    Create delete company command UML class diagram

--- src/main/java/seedu/address/logic/commands/util/CommandUtil.java
+++ src/main/java/seedu/address/logic/commands/util/CommandUtil.java
@@ -24,7 +24,7 @@ import seedu.address.ui.tabs.TabName;
 /**
  * Encapsulates common / shared execution processes between commands.
  */
-public class CommandUtil {
+public abstract class CommandUtil {
 
     public static CompanyItem getCompany(Model model, Index companyIndex) throws CommandException {
         List<CompanyItem> lastShownList = model.getCompanyList().getFilteredItemList();
