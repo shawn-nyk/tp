b782a1205795b2139dabd1456adc887b941e5464
keanecjy <e0425601@u.nus.edu>
2020-10-22 12:39:11 +0800

    Update requirement to hold any string

--- src/main/java/seedu/address/model/internship/Requirement.java
+++ src/main/java/seedu/address/model/internship/Requirement.java
@@ -1,84 +1,32 @@
 package seedu.address.model.internship;
 
-import static java.util.Objects.requireNonNull;
-import static seedu.address.commons.util.AppUtil.checkArgument;
+import seedu.address.model.wrapper.NonEmptyString;
 
 /**
  * Requirement is an optional field in an InternshipItem.
  * Guarantees: immutable; is valid as declared in {@link #isValidRequirement(String)}
  */
-public class Requirement {
+public class Requirement extends NonEmptyString {
 
-
-    public static final String MESSAGE_CONSTRAINTS = "Requirement have a maximum of 2 words and not be blank";
-
-    private static final int MAX_NUMBER_OF_WORDS = 2;
-    private final String value;
+    public static final String MESSAGE_CONSTRAINTS = "Requirements should not be blank";
 
     /**
-     * Constructs a {@code AlNumWord}.
+     * Constructs a {@code Requirement}.
      *
-     * @param requirement A valid word that has at least one non-space character.
+     * @param requirement A valid requirement.
      */
     public Requirement(String requirement) {
-        requireNonNull(requirement);
-        checkArgument(isValidRequirement(requirement), MESSAGE_CONSTRAINTS);
-        value = requirement;
+        super(requirement, MESSAGE_CONSTRAINTS);
     }
 
     /**
-     * Retrieves the Requirement string value.
+     * Returns true if a given string is a valid requirement.
      *
-     * @return String value in Requirement.
+     * @param requirement String to test.
+     * @return True if the given string is a valid requirement, false otherwise.
      */
-    public String getValue() {
-        return value;
-    }
-
-    /**
-     * Returns true if a given string contains 2 or less words.
-     *
-     * @param test String to test.
-     * @return True if the given string is not empty, false otherwise.
-     */
-    public static boolean isValidRequirement(String test) {
-        if (test.isBlank()) {
-            return false;
-        }
-        return test.strip().replaceAll("\\s{2,}", " ").split(" ").length <= MAX_NUMBER_OF_WORDS;
-    }
-
-    /**
-     * Returns the string representation of this Requirement object.
-     *
-     * @return String representation of this Requirement object.
-     */
-    @Override
-    public String toString() {
-        return value;
-    }
-
-    /**
-     * Returns true if the other Requirement object have the same value.
-     *
-     * @param other Object object to compare to.
-     * @return True if the other Requirement object has the same value as this object.
-     */
-    @Override
-    public boolean equals(Object other) {
-        return other == this // short circuit if same object
-                || (other instanceof Requirement // instanceof handles nulls
-                && value.equals(((Requirement) other).value)); // state check
-    }
-
-    /**
-     * Returns the hashcode of this Requirement object, which is the hashcode of the value.
-     *
-     * @return Hashcode of this Requirement object.
-     */
-    @Override
-    public int hashCode() {
-        return value.hashCode();
+    public static boolean isValidRequirement(String requirement) {
+        return isValidNonEmptyString(requirement);
     }
 
 }
