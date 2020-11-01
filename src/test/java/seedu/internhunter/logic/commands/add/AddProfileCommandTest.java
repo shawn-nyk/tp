271355179eae03ab06ea17debbf6e0e573b14f5e
orzymandias <ganwancheng@gmail.com>
2020-11-02 03:27:59 +0800

    Add profile command test

--- src/test/java/seedu/internhunter/logic/commands/add/AddProfileCommandTest.java
+++ src/test/java/seedu/internhunter/logic/commands/add/AddProfileCommandTest.java
@@ -1,97 +1,96 @@
 package seedu.internhunter.logic.commands.add;
 
+import static java.util.Objects.requireNonNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.internhunter.commons.core.Messages.MESSAGE_ADD_SUCCESS;
+import static seedu.internhunter.commons.core.Messages.MESSAGE_DUPLICATE_ITEM;
+import static seedu.internhunter.model.util.ItemUtil.PROFILE_ITEM_NAME;
+import static seedu.internhunter.model.util.ItemUtil.PROFILE_NAME;
 import static seedu.internhunter.testutil.Assert.assertThrows;
-import static seedu.internhunter.testutil.profile.SampleProfileItems.getSampleProfileItemList;
+import static seedu.internhunter.testutil.profile.ProfileItemFieldsUtil.VALID_TITLE_GOVTECH_INTERNSHIP;
+import static seedu.internhunter.testutil.profile.ProfileItemFieldsUtil.VALID_TITLE_HACKATHON;
 
-import org.junit.jupiter.api.BeforeEach;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Test;
 
-import seedu.internhunter.model.Model;
-import seedu.internhunter.model.ModelManager;
-import seedu.internhunter.model.UserPrefs;
-import seedu.internhunter.model.item.ItemList;
+import seedu.internhunter.logic.commands.CommandResult;
+import seedu.internhunter.logic.commands.exceptions.CommandException;
+import seedu.internhunter.model.profile.ProfileItem;
+import seedu.internhunter.testutil.profile.ProfileItemBuilder;
 
 public class AddProfileCommandTest {
-    private Model model;
-    private Model expectedModel;
 
-    @BeforeEach
-    public void setUp() {
-        model = new ModelManager(new ItemList<>(), new ItemList<>(), getSampleProfileItemList(),
-                new UserPrefs());
-        expectedModel = new ModelManager(new ItemList<>(), new ItemList<>(),
-                model.getUnfilteredProfileList(), new UserPrefs());
-    }
     @Test
     public void constructor_nullProfileItem_throwsNullPointerException() {
         assertThrows(NullPointerException.class, () -> new AddProfileCommand(null));
     }
 
-//    @Test
-//    public void execute_profileItemAcceptedByModel_addSuccessful() throws Exception {
-//        ProfileItem profileItem = new ProfileItemBuilder().build();
-//
-//        CommandResult commandResult = new AddProfileCommand(profileItem).execute(model);
-//
-//        assertEquals(String.format(AddCommand.MESSAGE_SUCCESS, profileItem), commandResult.getFeedbackToUser());
-//        assertEquals(Arrays.asList(profileItem), modelStub.personsAdded);
-//    }
-//
-//    @Test
-//    public void execute_duplicatePerson_throwsCommandException() {
-//        Person validPerson = new PersonBuilder().build();
-//        AddCommand addCommand = new AddCommand(validPerson);
-//        ModelStub modelStub = new ModelStubWithPerson(validPerson);
-//
-//        assertThrows(CommandException.class, AddCommand.MESSAGE_DUPLICATE_PERSON, () -> addCommand.execute(modelStub));
-//    }
-//
-//    @Test
-//    public void equals() {
-//        Person alice = new PersonBuilder().withName("Alice").build();
-//        Person bob = new PersonBuilder().withName("Bob").build();
-//        AddCommand addAliceCommand = new AddCommand(alice);
-//        AddCommand addBobCommand = new AddCommand(bob);
-//
-//        // same object -> returns true
-//        assertTrue(addAliceCommand.equals(addAliceCommand));
-//
-//        // same values -> returns true
-//        AddCommand addAliceCommandCopy = new AddCommand(alice);
-//        assertTrue(addAliceCommand.equals(addAliceCommandCopy));
-//
-//        // different types -> returns false
-//        assertFalse(addAliceCommand.equals(1));
-//
-//        // null -> returns false
-//        assertFalse(addAliceCommand.equals(null));
-//
-//        // different person -> returns false
-//        assertFalse(addAliceCommand.equals(addBobCommand));
-//    }
-//
-//
-//    /**
-//     * A Model stub that always accept the profile item being added.
-//     */
-//    private class ModelStubAcceptingProfileItemAdded extends ModelStub {
-//        final ArrayList<ProfileItem> profileItems = new ArrayList<>();
-//
-//        @Override
-//        public boolean hasProfileItem(ProfileItem profileItem) {
-//            requireNonNull(profileItem);
-//            return profileItems.stream().anyMatch(profileItem::isSameItem);
-//        }
-//
-//        @Override
-//        public void addProfileItem(ProfileItem profileItem) {
-//            requireNonNull(profileItem);
-//            profileItems.add(profileItem);
-//        }
-//
-//        @Override
-//        public ItemList getProfileItemList() {
-//            return new ItemList<ProfileItem>();
-//        }
-//    }
+    @Test
+    public void execute_profileItemAcceptedByModel_addSuccessful() throws Exception {
+        ModelStubAcceptingProfileItemAdded modelStub = new ModelStubAcceptingProfileItemAdded();
+        ProfileItem validProfileItem = new ProfileItemBuilder().build();
+
+        CommandResult commandResult = new AddProfileCommand(validProfileItem).execute(modelStub);
+
+        assertEquals(String.format(MESSAGE_ADD_SUCCESS, PROFILE_NAME, validProfileItem), commandResult.getFeedbackToUser());
+        assertEquals(Arrays.asList(validProfileItem), modelStub.profileItems);
+    }
+
+    @Test
+    public void execute_duplicatePerson_throwsCommandException() {
+        ProfileItem validProfileItem = new ProfileItemBuilder().build();
+        AddProfileCommand addProfileCommand = new AddProfileCommand(validProfileItem);
+        ModelStub modelStub = new ModelStubAcceptingProfileItemAdded();
+
+        assertThrows(CommandException.class, String.format(MESSAGE_DUPLICATE_ITEM, PROFILE_ITEM_NAME),
+            () -> addProfileCommand.execute(modelStub));
+    }
+
+    @Test
+    public void equals() {
+        ProfileItem internship = new ProfileItemBuilder().withTitle(VALID_TITLE_GOVTECH_INTERNSHIP).build();
+        ProfileItem hackathon = new ProfileItemBuilder().withTitle(VALID_TITLE_HACKATHON).build();
+        AddProfileCommand addProfileInternshipCommand = new AddProfileCommand(internship);
+        AddProfileCommand addProfileHackathonCommand = new AddProfileCommand(hackathon);
+
+        // same object -> returns true
+        assertTrue(addProfileInternshipCommand.equals(addProfileInternshipCommand));
+
+        // same values -> returns true
+        AddProfileCommand addInternshpCommandCopy = new AddProfileCommand(internship);
+        assertTrue(addProfileInternshipCommand.equals(addInternshpCommandCopy));
+
+        // different types -> returns false
+        assertFalse(addProfileInternshipCommand.equals(1));
+
+        // null -> returns false
+        assertFalse(addProfileInternshipCommand.equals(null));
+
+        // different person -> returns false
+        assertFalse(addProfileInternshipCommand.equals(addProfileHackathonCommand));
+    }
+
+
+    /**
+     * A Model stub that always accept the profile item being added.
+     */
+    private class ModelStubAcceptingProfileItemAdded extends ModelStub {
+        final ArrayList<ProfileItem> profileItems = new ArrayList<>();
+
+        @Override
+        public boolean hasProfileItem(ProfileItem profileItem) {
+            requireNonNull(profileItem);
+            return profileItems.stream().anyMatch(profileItem::isSameItem);
+        }
+
+        @Override
+        public void addProfileItem(ProfileItem profileItem) {
+            requireNonNull(profileItem);
+            profileItems.add(profileItem);
+        }
+    }
 }
