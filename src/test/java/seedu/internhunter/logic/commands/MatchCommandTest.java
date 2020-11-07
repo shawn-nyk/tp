4875ecd4eb85c3b99691bbb1482e442628eddd1e
keanecjy <e0425601@u.nus.edu>
2020-11-08 00:34:15 +0800

    Update tests for match command

--- src/test/java/seedu/internhunter/logic/commands/MatchCommandTest.java
+++ src/test/java/seedu/internhunter/logic/commands/MatchCommandTest.java
@@ -1,6 +1,7 @@
 package seedu.internhunter.logic.commands;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.internhunter.logic.commands.CommandTestUtil.showNoCompany;
 import static seedu.internhunter.logic.commands.CommandTestUtil.showNoProfile;
 import static seedu.internhunter.logic.commands.MatchCommand.NO_MATCHING_INTERNSHIPS_MESSAGE;
@@ -9,10 +10,11 @@ import static seedu.internhunter.testutil.company.SampleCompanyItems.getSampleCo
 import static seedu.internhunter.testutil.internship.SampleInternshipItems.FACEBOOK_SWE;
 import static seedu.internhunter.testutil.internship.SampleInternshipItems.GOLDMAN_BA;
 import static seedu.internhunter.testutil.profile.ProfileItemFieldsUtil.VALID_CATEGORY_ACHIEVEMENT;
-import static seedu.internhunter.testutil.profile.ProfileItemFieldsUtil.VALID_CATEGORY_EXPERIENCE;
 import static seedu.internhunter.testutil.profile.SampleProfileItems.HTML_SKILL;
 import static seedu.internhunter.testutil.profile.SampleProfileItems.R_SKILL;
 import static seedu.internhunter.testutil.profile.SampleProfileItems.getSampleProfileItemList;
+
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
@@ -28,22 +30,27 @@ import seedu.internhunter.testutil.profile.ProfileItemBuilder;
 
 /**
  * Contains integration tests (interaction with the Model) and unit tests for {@code MatchCommand}. Testing is done
- * through both command result since there is no changes to the model.
+ * through the {@link MatchCommandTest#assertMatchCommandSuccess(Model, CommandResult)} method since the match
+ * command does not change the model.
  */
 public class MatchCommandTest {
 
     private Model model;
     private MatchCommand matchCommand;
+    private CommandResult matchSuccessResult;
+    private CommandResult noMatchResult;
 
     @BeforeEach
     public void setUp() {
         model = new ModelManager(getSampleCompanyList(), new ItemList<>(), getSampleProfileItemList(),
                 new UserPrefs());
         matchCommand = new MatchCommand();
+        matchSuccessResult = new CommandResult(SHOWING_MATCH_COMMAND_MESSAGE);
+        noMatchResult = new CommandResult(NO_MATCHING_INTERNSHIPS_MESSAGE);
     }
 
     @Test
-    public void execute_noChangesToModel_successfulMatch() {
+    public void execute_noChangesToModel_success() {
         Model expectedModel = new ModelManager(getSampleCompanyList(), new ItemList<>(), getSampleProfileItemList(),
                 new UserPrefs());
         matchCommand.execute(model);
@@ -52,107 +59,110 @@ public class MatchCommandTest {
 
     @Test
     public void execute_onlyOneCompanyMatched_successfulMatch() {
-        CommandResult actualCommandResult = matchCommand.execute(model);
-        CommandResult expectedCommandResult = new CommandResult(SHOWING_MATCH_COMMAND_MESSAGE);
-        expectedCommandResult.setMatchingInternships(FXCollections.observableArrayList(FACEBOOK_SWE));
-        assertMatchCommandSuccess(actualCommandResult, expectedCommandResult, 1);
+        matchSuccessResult.setMatchingInternships(FXCollections.observableArrayList(FACEBOOK_SWE));
+        assertMatchCommandSuccess(model, matchSuccessResult);
     }
 
     @Nested
     class MultipleInternshipMatches {
 
-        @Test
-        public void execute_multipleCompaniesInternships_successfulMatch() {
+        @BeforeEach
+        public void setUp() {
             model.addProfileItem(R_SKILL);
-            CommandResult actualCommandResult = matchCommand.execute(model);
-            CommandResult expectedCommandResult = new CommandResult(SHOWING_MATCH_COMMAND_MESSAGE);
-            expectedCommandResult.setMatchingInternships(FXCollections.observableArrayList(GOLDMAN_BA, FACEBOOK_SWE));
-            assertMatchCommandSuccess(actualCommandResult, expectedCommandResult, 2);
+            assertTrue(GOLDMAN_BA.matches(List.of(R_SKILL.getTitleValue())));
+            matchSuccessResult.setMatchingInternships(FXCollections.observableArrayList(GOLDMAN_BA, FACEBOOK_SWE));
+        }
+
+        @Test
+        public void execute_unfilteredLists_successfulMatch() {
+            assertMatchCommandSuccess(model, matchSuccessResult);
         }
 
         @Test
         public void execute_filteredEmptyProfileList_successfulMatch() {
-            model.addProfileItem(R_SKILL);
             showNoProfile(model);
-            CommandResult actualCommandResult = matchCommand.execute(model);
-            CommandResult expectedCommandResult = new CommandResult(SHOWING_MATCH_COMMAND_MESSAGE);
-            expectedCommandResult.setMatchingInternships(FXCollections.observableArrayList(GOLDMAN_BA, FACEBOOK_SWE));
-            assertMatchCommandSuccess(actualCommandResult, expectedCommandResult, 2);
+            assertMatchCommandSuccess(model, matchSuccessResult);
         }
 
         @Test
         public void execute_filteredEmptyCompanyList_successfulMatch() {
-            model.addProfileItem(R_SKILL);
             showNoCompany(model);
-            CommandResult actualCommandResult = matchCommand.execute(model);
-            CommandResult expectedCommandResult = new CommandResult(SHOWING_MATCH_COMMAND_MESSAGE);
-            expectedCommandResult.setMatchingInternships(FXCollections.observableArrayList(GOLDMAN_BA, FACEBOOK_SWE));
-            assertMatchCommandSuccess(actualCommandResult, expectedCommandResult, 2);
+            assertMatchCommandSuccess(model, matchSuccessResult);
         }
 
         @Test
         public void execute_filteredBothLists_successfulMatch() {
-            model.addProfileItem(R_SKILL);
             showNoCompany(model);
             showNoProfile(model);
-            CommandResult actualCommandResult = matchCommand.execute(model);
-            CommandResult expectedCommandResult = new CommandResult(SHOWING_MATCH_COMMAND_MESSAGE);
-            expectedCommandResult.setMatchingInternships(FXCollections.observableArrayList(GOLDMAN_BA, FACEBOOK_SWE));
-            assertMatchCommandSuccess(actualCommandResult, expectedCommandResult, 2);
+            assertMatchCommandSuccess(model, matchSuccessResult);
+        }
+
+    }
+
+    @Nested
+    class NoInternshipMatches {
+
+        @BeforeEach
+        public void setUp() {
+            model.deleteProfileItem(HTML_SKILL);
+            // Asserts that there are no matching internships at this point
+            assertMatchCommandSuccess(model, noMatchResult);
         }
 
         @Test
-        public void execute_differentCategoryNonMatch_successfulMatch() {
-            // Checks for achievement category
-            model = new ModelManager(getSampleCompanyList(), new ItemList<>(), new ItemList<>(), new UserPrefs());
+        public void execute_achievementCategory_noMatchingInternships() {
             ProfileItem profileItemToAdd =
                     new ProfileItemBuilder(HTML_SKILL).withCategory(VALID_CATEGORY_ACHIEVEMENT).build();
             model.addProfileItem(profileItemToAdd);
-            CommandResult actualCommandResult = matchCommand.execute(model);
-            CommandResult expectedCommandResult = new CommandResult(NO_MATCHING_INTERNSHIPS_MESSAGE);
-            assertMatchCommandSuccess(actualCommandResult, expectedCommandResult, 0);
+            assertMatchCommandSuccess(model, noMatchResult);
+        }
 
-            // Checks for experience category
-            model = new ModelManager(getSampleCompanyList(), new ItemList<>(), new ItemList<>(), new UserPrefs());
-            profileItemToAdd = new ProfileItemBuilder(HTML_SKILL).withCategory(VALID_CATEGORY_EXPERIENCE).build();
+        @Test
+        public void execute_experienceCategory_noMatchingInternships() {
+            ProfileItem profileItemToAdd =
+                    new ProfileItemBuilder(HTML_SKILL).withCategory(VALID_CATEGORY_ACHIEVEMENT).build();
             model.addProfileItem(profileItemToAdd);
-            actualCommandResult = matchCommand.execute(model);
-            expectedCommandResult = new CommandResult(NO_MATCHING_INTERNSHIPS_MESSAGE);
-            assertMatchCommandSuccess(actualCommandResult, expectedCommandResult, 0);
+            assertMatchCommandSuccess(model, noMatchResult);
+        }
+    }
+
+    @Nested
+    class EmptyListsNoMatch {
+
+        @Test
+        public void execute_emptyCompanyList_noMatchingInternships() {
+            model = new ModelManager(new ItemList<>(), new ItemList<>(), getSampleProfileItemList(),
+                    new UserPrefs());
+            assertMatchCommandSuccess(model, noMatchResult);
+        }
+
+        @Test
+        public void execute_emptyProfileList_noMatchingInternships() {
+            model = new ModelManager(getSampleCompanyList(), new ItemList<>(), new ItemList<>(),
+                    new UserPrefs());
+            assertMatchCommandSuccess(model, noMatchResult);
+        }
+
+        @Test
+        public void execute_bothEmptyLists_noMatchingInternships() {
+            model = new ModelManager(new ItemList<>(), new ItemList<>(), new ItemList<>(),
+                    new UserPrefs());
+            assertMatchCommandSuccess(model, noMatchResult);
         }
 
     }
 
-    @Test
-    public void execute_emptyCompanyList_noMatchingInternships() {
-        model = new ModelManager(new ItemList<>(), new ItemList<>(), getSampleProfileItemList(),
-                new UserPrefs());
-        CommandResult actualCommandResult = matchCommand.execute(model);
-        CommandResult expectedCommandResult = new CommandResult(NO_MATCHING_INTERNSHIPS_MESSAGE);
-        assertMatchCommandSuccess(actualCommandResult, expectedCommandResult, 0);
-    }
-
-    @Test
-    public void execute_emptyProfileList_noMatchingInternships() {
-        model = new ModelManager(getSampleCompanyList(), new ItemList<>(), new ItemList<>(),
-                new UserPrefs());
-        CommandResult actualCommandResult = matchCommand.execute(model);
-        CommandResult expectedCommandResult = new CommandResult(NO_MATCHING_INTERNSHIPS_MESSAGE);
-        assertMatchCommandSuccess(actualCommandResult, expectedCommandResult, 0);
-    }
-
     /**
-     * Checks if the given command result is the same as the expected one. Also checks if the size of matching
-     * internships in the actualCommandResult is equal to the matchingInternshipsSize.
+     * Checks if the given command result is the same as the command result from the execution of the match command.
+     * Whether the 2 list of matching internships are equal is also tested through the equals method of the
+     * CommandResult class.
      *
+     * @param model Model used.
      * @param expectedCommandResult Expected CommandResult.
-     * @param actualCommandResult Actual CommandResult.
-     * @param matchingInternshipsSize Expected size of matching internships.
      */
-    private void assertMatchCommandSuccess(CommandResult expectedCommandResult, CommandResult actualCommandResult,
-            int matchingInternshipsSize) {
+    private void assertMatchCommandSuccess(Model model, CommandResult expectedCommandResult) {
+        CommandResult actualCommandResult = matchCommand.execute(model);
         assertEquals(expectedCommandResult, actualCommandResult);
-        assertEquals(matchingInternshipsSize, actualCommandResult.getMatchingInternships().size());
     }
 
 }
