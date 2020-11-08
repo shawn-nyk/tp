9ffc204939975df897c17786821e5b6a31426077
Sean <e0406739@u.nus.edu>
2020-11-09 01:38:27 +0800

    test if have to use .equals explicitly

--- src/test/java/seedu/internhunter/model/profile/DescriptorTest.java
+++ src/test/java/seedu/internhunter/model/profile/DescriptorTest.java
@@ -1,11 +1,14 @@
 package seedu.internhunter.model.profile;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.internhunter.testutil.profile.ProfileItemFieldsUtil.INVALID_BLANK_DESCRIPTOR;
 import static seedu.internhunter.testutil.profile.ProfileItemFieldsUtil.INVALID_DESCRIPTOR_AMPERSAND;
 import static seedu.internhunter.testutil.profile.ProfileItemFieldsUtil.INVALID_DESCRIPTOR_PARENTHESIS;
 import static seedu.internhunter.testutil.profile.ProfileItemFieldsUtil.VALID_DESCRIPTOR;
+import static seedu.internhunter.testutil.profile.ProfileItemFieldsUtil.VALID_DESCRIPTOR_GOVTECH;
 import static seedu.internhunter.testutil.profile.ProfileItemFieldsUtil.VALID_DESCRIPTOR_PUNCTUATION;
 
 import org.junit.jupiter.api.Test;
@@ -36,7 +39,21 @@ class DescriptorTest {
     public void equals_equalityTest_success() {
         Descriptor descriptorOne = new Descriptor(VALID_DESCRIPTOR);
         Descriptor descriptorTwo = new Descriptor(VALID_DESCRIPTOR);
-        assertEquals(descriptorOne, descriptorTwo);
+
+        // same object -> return true
+        assertTrue(descriptorOne.equals(descriptorOne));
+
+        // same value -> return true
+        assertTrue(descriptorOne.equals(descriptorTwo));
+
+        // null -> return false
+        assertFalse(descriptorOne.equals(null));
+
+        // different types -> return false
+        assertFalse(descriptorOne.equals(0.5f));
+
+        // different value -> return false
+        assertFalse(descriptorOne.equals(new Descriptor(VALID_DESCRIPTOR_GOVTECH)));
     }
 
     @Test
